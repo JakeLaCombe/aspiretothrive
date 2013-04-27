9a5058e4ea52c825c508a0ab610aad738dab0fd8
JakeLaCombe <jake.lacombe2@gmail.com>
2013-04-27 14:04:53 -0500

    Add registration pages

---
+++ registration.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Untitled Document</title>
+    <link href="Styles/styles.css" rel="stylesheet"/>
+  </head>
+  <body>
+    <div id="body-wrapper">
+          <h1 id="registration-header">You're only seconds away from a fresh start in life!</h1>
+    <section>
+        <div>
+       <ul id="registration-benefits">
+      	<li>Easy registration!</li>
+        <li>Lot's of mentors ready to help you!</li>
+        <li>Don't delay a Fresh start!</li>
+        <li><img src="Images/Teen-Mentoring.jpg" width="300" height="200"/></li>
+      </ul>
+    </div> 
+      <div>
+      <form id="registration-form">
+      <p>
+        <label for="first-name">First Name:</label><input id="first-name" type="text" name="first-name"/>
+      </p>
+      <p>
+        <label for="last-name">Last Name:</label><input id="last-name" type="text" name="last-name"/>
+      </p>
+            <p>
+        <label for="email">Email:</label><input id="email" type="email" name="email"/>
+      </p>
+      <p>
+        <label for="address">Address:</label><input id="address" type="text" name="address"/>
+      </p>
+      <p>
+        <label for="city">City:</label><input id="city" type="text" name="city"/>
+      </p>
+      <p>
+        <label for="zip">Zip:</label><input id="zip" type="text" name="zip"/>
+      </p>
+      <p>
+        <input type="submit" class="shifted-submit"/>
+      </p>
+      </form> 
+      </div>
+  	</section>
+   </div>
+ </body>
+</html>
