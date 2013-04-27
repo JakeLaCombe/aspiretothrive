853db13a0529e6989e3419a4d1f76a4aac0c915d
JakeLaCombe <jake.lacombe2@gmail.com>
2013-04-27 14:42:54 -0500

    Add mentee page

---
+++ mentee.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Untitled Document</title>
+    <link href="Styles/styles.css" rel="stylesheet"/>
+  </head>
+  <body>
+   <nav><img src="Images/aspire.png" width="100" height="100"/></nav>
+   <div id="body-wrapper">
+    <section> 
+      <div class="profile-block">
+        <img src="Images/ProfilePic.jpg" width="100" height="100" alt="Profile Image"/>
+        <div class="profile-information">
+        	<p>Name: Jake LaCombe</p>
+            <p>Active Since: May 2013</p>
+        </div>
+      </div>
+       <div id="mentor-information">
+       	  <h1>Mentor</h1>
+          <img src="Images/ProfilePic.jpg" width="100" height="100" alt="Profile Image"/>
+          <span>Jake LaCombe</span>
+        </div>
+      </section>
+      <section>
+        <div id="wall">
+          <h1>Wall</h1>
+          <article class="first_of_list">
+            <img src="Images/ProfilePic.jpg" width="100" height="100"/>
+            <div>
+              <h2>Jake LaCombe</h2>
+              <p>Way to keep it up buddy! That’s great you found a job as a car mechanic! You’ll do great, I’m sure of it!</p>
+            </div>
+          </article>
+          <article>
+            <img src="Images/ProfilePic.jpg" width="100" height="100"/>
+            <div>
+              <h2>Jake LaCombe</h2>
+              <p>Way to keep it up buddy! T</p>
+            </div>
+          </article>
+          <article>
+            <img src="Images/ProfilePic.jpg" width="100" height="100"/>
+            <div>
+              <h2>Jake LaCombe</h2>
+              <p>Way to keep it up buddy! That’s great you found a job as a car mechanic! You’ll do great, I’m sure of it!</p>
+            </div>
+          </article>
+          <article>
+            <img src="Images/ProfilePic.jpg" width="100" height="100"/>
+            <div>
+              <h2>Jake LaCombe</h2>
+              <p>Way to keep it up buddy! That’s great you found a job as a car mechanic! You’ll do great, I’m sure of it!</p>
+            </div>
+          </article>
+        </div>
+         <div id="team-mates">
+          <h1>Team Mates</h1>
+          <article class="first_of_list">
+            <img src="Images/ProfilePic.jpg" width="100" height="100"/>
+            <span>Jake LaCombe</span>
+          </article>
+          <article>
+            <img src="Images/ProfilePic.jpg" width="100" height="100"/>
+            <span>Jake LaCombe</span>
+          </article>
+        </div> 
+      </section>
+     </div>
+    </body>
+  </html>
